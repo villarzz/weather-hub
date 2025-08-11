2b2f5e3f4489a241223f3d798f7b3b018775a27d
hugo vilar <hugovilar04@gmail.com>
2025-08-11 15:09:42 -0300

    FEAT(layout) - atualizar estilo do componente App para melhorar responsividade

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,8 @@ import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <div className="bg-zinc-500/50 h-screen">
+    <div className="min-h-screen min-w-screen max-h-screen max-w-screen overflow-hidden bg-sky-700/50">
       <Home />
     </div>
-  )
+  );
 }
-
-
