4fa761721bf83905a4fc078df7019d13be5fb3e1
hugo vilar <hugovilar04@gmail.com>
2025-08-11 15:08:37 -0300

    FEAT(assets) - adicionando icones

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -3,9 +3,9 @@ import { CiSearch } from "react-icons/ci";
 export default function SearchInput() {
     return (
         <>
-            <div className="bg-zinc-600/50 p-2 rounded w-96 flex items-center gap-1">
-                <CiSearch className="text-2xl" />
-                <input type="text" placeholder="Procure por lugares" className="w-full p-1 rounded-sm focus:border-none focus:outline-none"/>
+            <div className="bg-sky-900/50 p-2 rounded-xl w-96 flex items-center gap-1">
+                <CiSearch className="text-3xl text-zinc-200" />
+                <input type="text" placeholder="Procure por lugares" className="w-full p-1 rounded-xl focus:border-none focus:outline-none placeholder-zinc-300/80"/>
             </div>
         </>
     )
