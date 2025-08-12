c4bf14b83a00a41e129f72191d71050c67f70c9e
hugo vilar <hugovilar04@gmail.com>
2025-08-12 08:18:33 -0300

    FEAT(home) - adicionar componente CurrentWeather à página Home para exibir informações climáticas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,14 @@
+import CurrentWeather from "../components/current-weather";
 import SearchInput from "../components/search-input";
+
 
 export default function Home() {
     return (
         <div className="pt-10 pl-12">
             <SearchInput />
+            <div className="mt-16">
+                <CurrentWeather />
+            </div>
         </div>
     )
 }
