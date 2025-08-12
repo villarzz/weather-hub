90faeccfe2e5360d35605c7baa073db19e488e4a
hugo vilar <hugovilar04@gmail.com>
2025-08-12 14:01:07 -0300

    FEAT(weather-popular-cities) - adicionar componente WeatherPopularCities para exibir condições climáticas em cidades populares

---
+++ src/components/weather-popular-cities.tsx
@@ -0,0 +1,44 @@
+import rainy from "../assets/Rainy-Bulk.svg";
+import snowy from "../assets/Snowy-Bulk.svg";
+import sunny from "../assets/Sunny-Bulk.svg";
+import rainThunder from "../assets/RainThunder-Bulk.svg";
+import partlyCloudy from "../assets/PartlyCloudy-Bulk.svg";
+
+export default function WeatherPopularCities() {
+    return (
+        <div className="w-[450px] h-[400px] rounded-xl bg-sky-900/50 p-6">
+            <p className="font-bold text-zinc-200 text-xl tracking-wide ml-1">Cidades Populares</p>
+            <div className="overflow-x-auto ">
+                <table className="table-auto border-collapse w-full">
+                    <tbody>
+                        <tr>
+                            <td className="p-2"><img src={partlyCloudy} width={50} /></td>
+                            <td className="text-zinc-100 p-2">Nova York</td>
+                            <td className="text-zinc-100 text-sm p-2 whitespace-nowrap text-right">Parcialmente Nublado</td>
+                        </tr>
+                        <tr>
+                            <td className="text-zinc-100 p-2"><img src={rainThunder} width={50} /></td>
+                            <td className="text-zinc-100 p-2">Paris</td>
+                            <td className="text-zinc-100 text-sm p-2 whitespace-nowrap text-right">Chuva forte</td>
+                        </tr>
+                        <tr>
+                            <td className="text-zinc-100 p-2"><img src={rainy} width={50} /></td>
+                            <td className="text-zinc-100 p-2">Tóquio</td>
+                            <td className="text-zinc-100 text-sm p-2 whitespace-nowrap text-right">Chuva</td>
+                        </tr>
+                        <tr>
+                            <td className="text-zinc-100 p-2"><img src={snowy} width={50} /></td>
+                            <td className="text-zinc-100 p-2">Londres</td>
+                            <td className="text-zinc-100 text-sm p-2 whitespace-nowrap text-right">Neve</td>
+                        </tr>
+                        <tr>
+                            <td className="text-zinc-100 p-2"><img src={sunny} width={50} /></td>
+                            <td className="text-zinc-100 p-2">Rio de Janeiro</td>
+                            <td className="text-zinc-100 text-sm p-2 whitespace-nowrap text-right">Ensolarado</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
