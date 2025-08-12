39052a9e9df3d7c87a29cbfe569ed96010801c69
hugo vilar <hugovilar04@gmail.com>
2025-08-12 15:26:05 -0300

    FEAT(forecast) - adicionar componente Forecast para exibir previsões climáticas com ícones correspondentes

---
+++ src/components/forecast.tsx
@@ -0,0 +1,42 @@
+import rainy from "../assets/Rainy-Bulk.svg";
+import snowy from "../assets/Snowy-Bulk.svg";
+import sunny from "../assets/Sunny-Bulk.svg";
+import rainThunder from "../assets/RainThunder-Bulk.svg";
+import partlyCloudy from "../assets/PartlyCloudy-Bulk.svg";
+
+export default function Forecast() {
+    return (
+        <div className="w-[400px] h-[450px] rounded-xl bg-sky-900/50 p-5 flex flex-col justify-evenly">
+            <p className="text-zinc-100 text-lg font-semibold">Previsões</p>
+            <table className="table-auto border-collapse w-full">
+                <tbody>
+                    <tr>
+                        <td className="p-2"><img src={partlyCloudy} width={50} /></td>
+                        <td className="text-zinc-100 p-2">24°/22°</td>
+                        <td className="text-zinc-100 text-sm p-2 whitespace-nowrap text-center"><span className="font-bold">12</span> Jul, Ter</td>
+                    </tr>
+                    <tr>
+                        <td className="text-zinc-100 p-2"><img src={rainThunder} width={50} /></td>
+                        <td className="text-zinc-100 p-2">24°/22°</td>
+                        <td className="text-zinc-100 text-sm p-2 whitespace-nowrap text-center"><span className="font-bold">13</span> Jul, Qua</td>
+                    </tr>
+                    <tr>
+                        <td className="text-zinc-100 p-2"><img src={rainy} width={50} /></td>
+                        <td className="text-zinc-100 p-2">24°/22°</td>
+                        <td className="text-zinc-100 text-sm p-2 whitespace-nowrap text-center"><span className="font-bold">14</span> Jul, Qui</td>
+                    </tr>
+                    <tr>
+                        <td className="text-zinc-100 p-2"><img src={snowy} width={50} /></td>
+                        <td className="text-zinc-100 p-2">24°/22°</td>
+                        <td className="text-zinc-100 text-sm p-2 whitespace-nowrap text-center"><span className="font-bold">15</span> Jul, Sex</td>
+                    </tr>
+                    <tr>
+                        <td className="text-zinc-100 p-2"><img src={sunny} width={50} /></td>
+                        <td className="text-zinc-100 p-2">24°/22°</td>
+                        <td className="text-zinc-100 text-sm p-2 whitespace-nowrap text-center"><span className="font-bold">16</span> Jul, Sab</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
