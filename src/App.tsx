c761e5e6045d93c40c111c721c349cac3e62c2ec
hugo vilar <hugovilar04@gmail.com>
2025-08-12 15:25:56 -0300

    FEAT(app) - permitir rolagem vertical na tela ao ajustar overflow para 'overflow-x-hidden'

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <div className="min-h-screen min-w-screen max-h-screen max-w-screen overflow-hidden bg-sky-900/50">
+    <div className="min-h-screen min-w-screen max-h-screen max-w-screen overflow-x-hidden bg-sky-900/50">
       <Home />
     </div>
   );
