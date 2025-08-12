183bfb3772c8035ab255faf6e27a80a90ba36856
hugo vilar <hugovilar04@gmail.com>
2025-08-12 15:26:20 -0300

    FEAT(home) - adicionar componente Forecast à página Home para exibir previsões climáticas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,21 @@
-
 import SearchInput from "../components/search-input";
 import LocationMap from "../components/location-map";
-
 import CurrentWeather from "../components/current-weather";
 import WeatherPopularCities from "../components/weather-popular-cities";
+import Forecast from "../components/forecast";
 
 
 export default function Home() {
     return (
-        <div className="pt-12 pl-16">
+        <div className="pt-8 pl-16 flex flex-col">
             <SearchInput />
-            <div className="mt-16 flex gap-10">
+            <div className="mt-10 flex gap-8">
                 <CurrentWeather />
                 <LocationMap cordx={-15.82686123202135} cordy={-47.92768564569077} />
                 <WeatherPopularCities />
+            </div>
+            <div className="flex mt-5 mb-10">
+                <Forecast />
             </div>
         </div>
     )
