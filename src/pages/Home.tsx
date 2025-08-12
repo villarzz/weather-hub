eb19a5ca075ef28a299d8ae5d3a2c333e784b8d0
hugo vilar <hugovilar04@gmail.com>
2025-08-12 09:19:47 -0300

    FEAT(home) - adicionar componente LocationMap à página Home para exibir um mapa interativo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,14 @@
-import CurrentWeather from "../components/current-weather";
 import SearchInput from "../components/search-input";
-
+import CurrentWeather from "../components/current-weather";
+import LocationMap from "../components/location-map";
 
 export default function Home() {
     return (
-        <div className="pt-10 pl-12">
+        <div className="pt-12 pl-16">
             <SearchInput />
-            <div className="mt-16">
+            <div className="mt-16 flex gap-10">
                 <CurrentWeather />
+                <LocationMap cordx={-15.82686123202135} cordy={-47.92768564569077} />
             </div>
         </div>
     )
