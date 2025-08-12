a15f60a4555c1bec70b775c6ef8da52d6ec055a1
hugo vilar <hugovilar04@gmail.com>
2025-08-12 08:18:26 -0300

    FEAT(weather) - adicionar componente CurrentWeather para exibir informações climáticas atuais

---
+++ src/components/current-weather.tsx
@@ -0,0 +1,34 @@
+import { LuWind } from "react-icons/lu";
+import icon from "../assets/PartlyCloudy.svg";
+import { IoWaterOutline } from "react-icons/io5";
+
+export default function CurrentWeather() {
+    return (
+        <div className="w-[400px] h-[400px] rounded-xl bg-sky-900/50 pl-3 flex flex-col justify-evenly">
+            <div className="mt-3 pl-4">
+                <p className="font-bold text-zinc-200 text-xl tracking-wide">Clima Atual</p>
+                <p className="text-zinc-200">2:14 PM</p>
+            </div>
+            <div className="flex items-center px-5 gap-16">
+                <img src={icon} alt="" width={150} />
+                <div className="flex flex-col text-center">
+                    <div className="flex">
+                        <p className="text-8xl font-semibold text-zinc-100 leading-none">24</p>
+                        <span className="text-zinc-100 text-2xl font-semibold ml-1 mt-1">°C</span>
+                    </div>
+                    <p className="font-semibold text-zinc-100">Ensolarado</p>
+                </div>
+            </div>
+            <div className="mx-3 flex justify-around p-2">
+                <div className="flex flex-col items-center gap-1">
+                    <IoWaterOutline className="text-zinc-200 text-3xl" />
+                    <p className="text-white">30%</p>
+                </div>
+                <div className="flex flex-col items-center gap-1">
+                    <LuWind className="text-zinc-200 text-3xl" />
+                    <p className="text-white">6 km/h</p>
+                </div>
+            </div>
+        </div>
+    )
+}
