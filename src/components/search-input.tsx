9e450e22d12c192908335741a78df601f56ec254
hugo vilar <hugovilar04@gmail.com>
2025-08-12 15:26:13 -0300

    FEAT(search-input) - aumentar a largura do campo de entrada de busca para 400px

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -3,7 +3,7 @@ import { CiSearch } from "react-icons/ci";
 export default function SearchInput() {
     return (
         <>
-            <div className="bg-sky-900/50 p-2 rounded-xl w-96 flex items-center gap-1">
+            <div className="bg-sky-900/50 p-2 rounded-xl w-[400px] flex items-center gap-1">
                 <CiSearch className="text-3xl text-zinc-200" />
                 <input type="text" placeholder="Procure por lugares" className="w-full p-1 rounded-xl focus:border-none focus:outline-none placeholder-zinc-300/80"/>
             </div>
