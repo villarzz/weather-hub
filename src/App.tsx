feedcd6d625441931ea8560870521280305f8761
hugo vilar <hugovilar04@gmail.com>
2025-08-14 11:31:07 -0300

    FEAT(app) - envolvendo componente Home no contexto de informações do clima

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Home from "./pages/Home";
+import { WeatherProvider } from "./context/WeatherProvider";
 
 export default function App() {
   return (
     <div className="min-h-screen min-w-screen max-h-screen max-w-screen overflow-x-hidden bg-sky-900/50">
-      <Home />
+      <WeatherProvider>
+        <Home />
+      </WeatherProvider>
     </div>
   );
 }
