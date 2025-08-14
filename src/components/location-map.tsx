1d1d3d15a0a41e05f38d01909260bb934b29a60a
hugo vilar <hugovilar04@gmail.com>
2025-08-14 15:38:32 -0300

    FIX(location-map) - arrumando centralização do mapa

--- src/components/location-map.tsx
+++ src/components/location-map.tsx
@@ -1,14 +1,20 @@
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 
-export default function LocationMap({cordx, cordy}: {cordx: number, cordy: number}) {
+export default function LocationMap({ cordx, cordy }: { cordx: number, cordy: number }) {
     const hyderabadCoords: [number, number] = [cordx, cordy];
+
+    function ChangeMapView({ coords }: { coords: [number, number] }) {
+        const map = useMap();
+        map.setView(coords, map.getZoom());
+        return null;
+    }
 
     return (
         <div className="bg-white/10 backdrop-blur-lg rounded-xl overflow-hidden shadow-lg p-0 h-[400px] w-full">
             <MapContainer
                 center={hyderabadCoords}
-                zoom={14}
+                zoom={15}
                 style={{ height: "100%", width: "100%" }}
                 className="rounded-xl"
             >
@@ -16,9 +22,12 @@ export default function LocationMap({cordx, cordy}: {cordx: number, cordy: numbe
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+
                 <Marker position={hyderabadCoords}>
                     <Popup>Hyderabad - Heavy Rain</Popup>
                 </Marker>
+
+                <ChangeMapView coords={hyderabadCoords} />
             </MapContainer>
         </div>
     )
