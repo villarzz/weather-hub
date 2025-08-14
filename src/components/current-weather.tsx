daea2603454cab2d49c7d291dc69b14bf9b02cd4
hugo vilar <hugovilar04@gmail.com>
2025-08-14 15:33:17 -0300

    FEAT(weather) - retirando dadosfixos e  uitilizando informações do contexto recebidas pela api para mostrar dados dinamicos

--- src/components/current-weather.tsx
+++ src/components/current-weather.tsx
@@ -1,32 +1,64 @@
 import { LuWind } from "react-icons/lu";
-import icon from "../assets/PartlyCloudy.svg";
+import Sunny from "../assets/Sunny.svg";
+import Rainy from "../assets/Rainy.svg";
+import Snowy from "../assets/Snowy.svg";
 import { IoWaterOutline } from "react-icons/io5";
+import RainThunder from "../assets/RainThunder.svg";
+import PartlyCloudy from "../assets/PartlyCloudy.svg";
+import { useWeather } from "../context/WeatherProvider";
 
 export default function CurrentWeather() {
+    const { loading, weatherData } = useWeather();
+
+    if (loading || !weatherData) {
+        return (
+            <div className="h-[400px] rounded-xl bg-sky-900/50 animate-pulse" />
+        )
+    }
+
+    function formatarData(dataStr: string): string {
+        const [data, hora] = dataStr.split(" ");
+        const [ano, mes, dia] = data.split("-");
+
+        return `${dia}/${mes}/${ano} ${hora}`;
+    }
+
+    const conditionIcons: Record<string, string> = {
+        "Parcialmente nublado": PartlyCloudy,
+        "Sunny": Sunny,
+        "Rain": Rainy,
+        "Snowy": Snowy,
+        "RainThunder": RainThunder
+    };
+    const conditionText = weatherData.current.condition.text;
+    const weatherIcon = conditionIcons[conditionText];
+
     return (
         <div className="h-[400px] rounded-xl bg-sky-900/50 flex flex-col justify-evenly">
             <div className="pl-4">
                 <p className="font-bold text-zinc-200 text-xl tracking-wide">Clima Atual</p>
-                <p className="text-zinc-200">2:14 PM</p>
+                <p className="text-zinc-200">{formatarData(weatherData.location.localtime)}</p>
             </div>
             <div className="flex items-centers gap-9">
-                <img src={icon} alt="" width={150}  className="ml-2"/>
-                <div className="flex flex-col text-center">
+                <img src={weatherIcon} alt="" width={150} className="ml-2" />
+                <div className="flex flex-col text-center items-center">
                     <div className="flex">
-                        <p className="text-8xl font-semibold text-zinc-100 leading-none">24</p>
+                        <p className="text-8xl font-semibold text-zinc-100 leading-none">
+                            {Math.floor(weatherData.current.temp_c)}
+                        </p>
                         <span className="text-zinc-100 text-2xl font-semibold ml-1 mt-1">°C</span>
                     </div>
-                    <p className="font-semibold text-zinc-100">Ensolarado</p>
+                    <p className="font-semibold text-zinc-100">{weatherData.current.condition.text}</p>
                 </div>
             </div>
             <div className="mx-3 flex justify-around p-2">
                 <div className="flex flex-col items-center gap-1">
                     <IoWaterOutline className="text-zinc-200 text-3xl" />
-                    <p className="text-white">30%</p>
+                    <p className="text-white">{weatherData.current.humidity}%</p>
                 </div>
                 <div className="flex flex-col items-center gap-1">
                     <LuWind className="text-zinc-200 text-3xl" />
-                    <p className="text-white">6 km/h</p>
+                    <p className="text-white">{weatherData.current.wind_kph} km/h</p>
                 </div>
             </div>
         </div>
