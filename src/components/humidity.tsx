eedaf64efd90145a7c54e31a95aee32cd37944b1
hugo vilar <hugovilar04@gmail.com>
2025-08-15 11:30:06 -0300

    FEAT(humidity) - implementa exibição dinâmica da umidade com base nos dados climáticos

--- src/components/humidity.tsx
+++ src/components/humidity.tsx
@@ -1,21 +1,37 @@
 import humidity from "../../public/assets/humidity.png";
+import { useWeather } from "../context/WeatherProvider";
 
 export default function Humidity() {
+    const { loading, weatherData } = useWeather();
+
+    if(loading || !weatherData){
+        return (
+            <div className="h-[209px] rounded-xl bg-sky-900/50 p-5 animate-pulse"/>
+        );
+    }
+
     return (
         <div className="h-[209px] rounded-xl bg-sky-900/50 p-5 flex flex-col items-start space-x-4">
             <div className="flex gap-5 items-center">
                 <img src={humidity} alt="Ícone de umidade" className="w-16 h-16 flex-shrink-0" />
                 <span>
                     <p className="text-zinc-100 text-xl font-semibold">Umidade do ar</p>
-                    <p className="text-zinc-200/80">Alta</p>
+                    <p className="text-zinc-200/80">
+                        {weatherData.current.humidity < 40
+                            ? "Baixa"
+                            : weatherData.current.humidity <= 70
+                            ? "Média"
+                            : "Alta"}
+                    </p>
                 </span>
             </div>
             <div className="flex items-center justify-center w-full h-full">
                 <p className="text-zinc-100 font-semibold text-center">
-                    <span className="font-semibold text-7xl">63</span>
+                    <span className="font-semibold text-7xl">{weatherData.current.humidity}</span>
                     <span className="text-5xl">%</span>
                 </p>
             </div>
         </div>
-    )
+    );
+    
 }
