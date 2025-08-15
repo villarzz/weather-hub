c28dcd125d2c261bc54d65bfd3365bc0f5a9dbf4
hugo vilar <hugovilar04@gmail.com>
2025-08-15 11:36:29 -0300

    FEAT(sunrise) - implementa exibição dinâmica do nascer do sol com formatação de data

--- src/components/sunrise.tsx
+++ src/components/sunrise.tsx
@@ -1,20 +1,30 @@
 import sunrise from "../../public/assets/sunrise.png";
+import { useWeather } from "../context/WeatherProvider";
+import { formatarData } from "../utils/utils";
 
 export default function Sunrise() {
+    const { loading, weatherData } = useWeather();
+
+    if (loading || !weatherData) {
+        return (
+            <div className="h-[209px] rounded-xl bg-sky-900/50 p-5 animate-pulse" />
+        );
+    }
+
     return (
         <div className="h-[209px] rounded-xl bg-sky-900/50 p-5 flex flex-col items-start space-x-4">
             <div className="flex gap-5 items-center">
                 <img src={sunrise} alt="Ícone de umidade" className="w-16 h-16 flex-shrink-0" />
                 <span>
                     <p className="text-zinc-100 text-2xl font-semibold whitespace-nowrap">Nascer do sol</p>
-                    <p className="text-zinc-100">14/08/2025</p>
+                    <p className="text-zinc-100">{formatarData(weatherData.location.localtime)}</p>
                 </span>
             </div>
             <div className="flex items-center justify-center w-full h-full">
                 <div className="flex flex-col text-center">
                     <div className="flex">
                         <p className="text-7xl font-semibold text-zinc-100 leading-none">
-                            6:29
+                            {weatherData.forecast.forecastday[0].astro.sunrise.split(" ")[0]}
                             <span className="text-3xl"> AM</span>
                         </p>
                     </div>
