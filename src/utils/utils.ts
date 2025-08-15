992e048ce9d16d8af285f05d7f102d3c4c980078
hugo vilar <hugovilar04@gmail.com>
2025-08-15 11:36:32 -0300

    FEAT(utils) - renomeia e aprimora função de formatação de data e hora

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,10 +1,17 @@
-export function formatarData(dataStr: string): string {
+export function formatarDataEHora(dataStr: string): string {
     const [data, hora] = dataStr.split(" ");
     const [ano, mes, dia] = data.split("-");
 
-    if(hora){
+    if (hora) {
         return `${dia}/${mes}/${ano} ${hora}`;
-    }else{
+    } else {
         return `${dia}/${mes}/${ano}`;
     }
+}
+
+export function formatarData(dataStr: string): string {
+    const [data] = dataStr.split(" ");
+    const [ano, mes, dia] = data.split("-");
+    
+    return `${dia}/${mes}/${ano}`;
 }
