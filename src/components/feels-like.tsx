b0ea520b610c2181af4b23b5c1d50f97ce801762
hugo vilar <hugovilar04@gmail.com>
2025-08-18 10:57:51 -0300

    FEAT(feels-like) - implementa exibição dinâmica da temperatura "sensação térmica" com base nos dados climáticos

--- src/components/feels-like.tsx
+++ src/components/feels-like.tsx
@@ -1,6 +1,13 @@
 import thermometer from "../../public/assets/atmospheric-conditions.png"
+import { useWeather } from "../context/WeatherProvider";
 
 export default function FeelsLike() {
+    const { loading, weatherData } = useWeather();
+
+    if (loading || !weatherData) {
+        return (<div className="h-[209px] rounded-xl bg-sky-900/50 p-5 flex animate-pulse" />)
+    }
+
     return (
         <div className="h-[209px] rounded-xl bg-sky-900/50 p-5 flex flex-col items-start space-x-4">
             <div className="flex gap-5 items-center">
@@ -13,7 +20,7 @@ export default function FeelsLike() {
             <div className="flex items-center justify-center w-full h-full">
                 <div className="flex flex-col text-center">
                     <div className="flex">
-                        <p className="text-7xl font-semibold text-zinc-100 leading-none">19</p>
+                        <p className="text-7xl font-semibold text-zinc-100 leading-none">{Math.floor(weatherData.current.feelslike_c)}</p>
                         <span className="text-zinc-100 text-2xl font-semibold ml-1 mt-1">°C</span>
                     </div>
                 </div>
